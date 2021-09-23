14711f4739293a183c9f515290901fca99df9fbe
chenjinhuo <chenjinhuo@bytedance.com>
2021-09-23 09:26:06 +0800

    feat:update @mitojs

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-## 概览
-Experience @mitojs/web in Vue2.6
+## overview
+Experience @mitojs/vue and @mitojs/browser in Vue2.6
 
-### 复用线上breadcrumb
 reuse [mito-admin-demo/breadcrumb-demo](https://github.com/mitojs/mito-admin-demo/blob/master/src/modules/breadcrumb-demo/views/index.tsx),create iframe and embed url,communicate with postMessage
--- src/main.js
+++ src/main.js
@@ -4,17 +4,21 @@ import router from '@/router'
 import './plugins/element'
 import '@/assets/css/index.css'
 import store from './store'
-import { init } from '@mitojs/vue'
+import { vuePlugin } from '@mitojs/vue'
+import { init } from '@mitojs/browser'
 
-const MitoInstance = init({
-  // apikey: '7b984e70-5d2e-11eb-a1d3-af21e7d909b7',
-  vue: Vue,
-  dsn: 'https://test.com',
-  beforeDataReport(data) {
-    return false
+const MitoInstance = init(
+  {
+    // apikey: '7b984e70-5d2e-11eb-a1d3-af21e7d909b7',
+    vue: Vue,
+    dsn: 'https://test.com',
+    beforeDataReport(data) {
+      return false
+    },
+    maxBreadcrumbs: 100
   },
-  maxBreadcrumbs: 100
-})
+  [vuePlugin]
+)
 window._MITO_ = MitoInstance
 new Vue({
   router,
